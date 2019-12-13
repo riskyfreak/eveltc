0d9dac73eed6cf473efebfbd41466230a0b063dd
Muhammad Risky <56730687+riskyfreak@users.noreply.github.com>
2019-12-13 19:05:36 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?
 
-$UserAgent = "xxxxxxx";
-$Email = "xxxxxxx";
+$UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0";
+$Email = "xxxxxxxxxx";
 $Password = "xxxxxxx";
 
 //Masukkan Cookie -> cf_clearance atau PHPSESSID
-$Cookie = "xxxxxxxx";
+$Cookie = "0hgoq9ck1ji6asvblbk1hrgt04";
